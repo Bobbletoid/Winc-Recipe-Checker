201b043e180d6622fa3f86d327ffd36dcd10a0be
Bobbletoid <vdk.Robin@hotmail.com>
2026-07-04 12:41:47 +0200

    Update RecipeListPage.jsx
    Added "No recipes found" when search criteria have no match
    Minor tweaks

--- src/pages/RecipeListPage.jsx
+++ src/pages/RecipeListPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Input, SimpleGrid, VStack } from "@chakra-ui/react";
+import { Center, Input, SimpleGrid, Text, VStack } from "@chakra-ui/react";
 import { RecipeCard } from "../components/RecipeCard";
 import { data } from "../utils/data";
 
@@ -8,22 +8,40 @@ export const RecipeListPage = ({ clickFn }) => {
 
   const filteredRecipes = data.hits.filter(({ recipe }) => {
     const searchTerm = search.toLowerCase();
-
     return recipe.label?.toLowerCase().includes(searchTerm);
   });
 
   return (
-    <VStack spacing={6} align="stretch">
+    <VStack gap="6" width="100%">
       <Input
         placeholder="Search recipe..."
+        _placeholder={{ _dark: { color: "white" } }}
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         w={{ base: "100%", sm: "30%" }}
         mx="auto"
         mb={6}
-        bg={{ _light: "white", _dark: "dimgray" }}
+        bg={{ _light: "white", _dark: "gray.400" }}
       />
-      <SimpleGrid
+
+      {filteredRecipes.length === 0
+      ? (
+        <Center p="10" flexDirection="column" gap="2">
+          <Text fontSize="xl"
+                fontWeight="bold"
+                color={{ _light: "gray.800", _dark: "gray.600" }}
+          >
+            No recipes found
+          </Text>
+          <Text fontSize="sm"
+                color={{ _light: "gray.700", _dark: "gray.400" }}
+          >
+            Try adjusting your search terms.
+          </Text>
+        </Center>
+        )
+      : (
+        <SimpleGrid
         columns={{ base: 1, sm: 2, md: 3, lg: 4 }}
         gap={{ base: 4, md: 6 }}
       >
@@ -35,6 +53,8 @@ export const RecipeListPage = ({ clickFn }) => {
           />
         ))}
       </SimpleGrid>
+      )}
     </VStack>
   );
 };
+
